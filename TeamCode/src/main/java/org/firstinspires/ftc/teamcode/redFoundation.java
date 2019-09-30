e9065219d6136a411ee11f4118c33f3f406fbb10
riceangel <43398049+riceangel@users.noreply.github.com>
2019-09-30 13:00:30 -0700

    Started Auto for Red Foundation Side

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/redFoundation.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name ="redFoundation")
+public class redFoundation extends LinearOpMode {
+
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+
+        waitForStart();
+
+        
+    }
+}
