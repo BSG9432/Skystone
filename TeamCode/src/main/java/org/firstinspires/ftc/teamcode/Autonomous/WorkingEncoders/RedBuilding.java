82b332788dc54c9e77b8ef02833c417ecb5330a6
Tyla Robertson <36782885+Tylasian@users.noreply.github.com>
2019-12-06 12:48:02 -0800

    updated RedBuilding with working BlueBuilding Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/WorkingEncoders/RedBuilding.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/WorkingEncoders/RedBuilding.java
@@ -1,33 +1,33 @@
 /* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-package org.firstinspires.ftc.teamcode.Autonomous.WorkingEncoders;
+        * Redistribution and use in source and binary forms, with or without modification,
+        * are permitted (subject to the limitations in the disclaimer below) provided that
+        * the following conditions are met:
+        *
+        * Redistributions of source code must retain the above copyright notice, this list
+        * of conditions and the following disclaimer.
+        *
+        * Redistributions in binary form must reproduce the above copyright notice, this
+        * list of conditions and the following disclaimer in the documentation and/or
+        * other materials provided with the distribution.
+        *
+        * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+        * promote products derived from this software without specific prior written permission.
+        *
+        * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+        * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+        * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+        * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+        * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+        * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+        * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+        * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+        * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+        * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+        * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        */
+
+        package org.firstinspires.ftc.teamcode.Autonomous.WorkingEncoders;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -109,8 +109,8 @@ public class RedBuilding extends LinearOpMode {
                 bsgRobot.backRight.getCurrentPosition());
         telemetry.update();
 
-        bsgRobot.rightFoundation.setPosition(0);
-        bsgRobot.leftFoundation.setPosition(1);
+        bsgRobot.rightFoundation.setPosition(1);
+        bsgRobot.leftFoundation.setPosition(0);
 
 
         // Wait for the game to start (driver presses PLAY)
@@ -120,22 +120,22 @@ public class RedBuilding extends LinearOpMode {
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
 
         //fix
-        strafeRight(1000);
+        strafeRight(800);
 //36.4 38
-        encoderDrive(.5, 47, 47, 6); //forward 40 inches towards foundation
+        encoderDrive(.5, 35, 35, 6); //forward 40 inches towards foundation
 
         sleep(500);
 
         foundationDown(2000); //grab foundation
 
-        encoderDrive(.8, -47, -47, 6); //drag foundation backwards 40 inches into build zone
+        encoderDrive(1, -35, -35, 6); //drag foundation backwards 40 inches into build zone
 
         sleep(500);
 
         foundationUp(800); //let go of foundation
 
         //fix
-        strafeLeft(3100);
+        strafeLeft(2000);
 
 
         //rotate(-90, .8); //rotate LEFT to face towards alliance bridge
@@ -285,14 +285,14 @@ public class RedBuilding extends LinearOpMode {
     }
 
     public void foundationDown(int pause) {
-        bsgRobot.rightFoundation.setPosition(.8);
-        bsgRobot.leftFoundation.setPosition(.2);
+        bsgRobot.rightFoundation.setPosition(.2);
+        bsgRobot.leftFoundation.setPosition(.8);
         sleep(pause);
     }
 
     public void foundationUp(int pause) {
-        bsgRobot.rightFoundation.setPosition(.1);
-        bsgRobot.leftFoundation.setPosition(.9);
+        bsgRobot.rightFoundation.setPosition(1);
+        bsgRobot.leftFoundation.setPosition(0);
         sleep(pause);
     }
 
