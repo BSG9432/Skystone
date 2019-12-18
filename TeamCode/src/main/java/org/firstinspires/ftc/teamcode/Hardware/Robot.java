63b34653f417fc1088941792ca14c583f90b216e
riceangel <43398049+riceangel@users.noreply.github.com>
2019-12-17 16:54:27 -0800

    Added Strafing Functions to Robot Class; Removed encoderDrive function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Robot.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -18,7 +21,9 @@ import java.util.Locale;
 
 //NOTE: This is off the top of my head
 
-public class Robot {
+@TeleOp
+@Autonomous
+public class Robot extends LinearOpMode {
   public DcMotor frontLeft;
   public DcMotor frontRight;
   public DcMotor backLeft;
@@ -39,6 +44,11 @@ public class Robot {
   public static Telemetry telemetry;
 
   public Robot() { //constructor
+
+  }
+
+  @Override
+  public void runOpMode() throws InterruptedException {
 
   }
 
@@ -89,74 +99,6 @@ public class Robot {
     // Set up our telemetry dashboard
     composeTelemetry();
 
-  }
-
-  public void encoderDrive(double speed,
-                           double leftInches, double rightInches,
-                           double timeoutS) {
-    int newLeftTarget;
-    int newRightTarget;
-
-    // Ensure that the opmode is still active
-    if (opModeIsActive()) {
-
-      // Determine new target position, and pass to motor controller
-      newLeftTarget = bsgRobot.frontLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
-      newLeftTarget = bsgRobot.backLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
-      newRightTarget = bsgRobot.frontRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
-      newRightTarget = bsgRobot.backRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
-
-      bsgRobot.frontLeft.setTargetPosition(newLeftTarget);
-      bsgRobot.backLeft.setTargetPosition(newLeftTarget);
-      bsgRobot.frontRight.setTargetPosition(newRightTarget);
-      bsgRobot.backRight.setTargetPosition(newRightTarget);
-
-
-      // Turn On RUN_TO_POSITION
-      bsgRobot.frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      bsgRobot.backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      bsgRobot.frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-      bsgRobot.backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-      // reset the timeout time and start motion.
-      runtime.reset();
-      bsgRobot.frontLeft.setPower(Math.abs(speed));
-      bsgRobot.backLeft.setPower(Math.abs(speed));
-      bsgRobot.frontRight.setPower(Math.abs(speed));
-      bsgRobot.backRight.setPower(Math.abs(speed));
-
-      // keep looping while we are still active, and there is time left, and both motors are running.
-      // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-      // its target position, the motion will stop.  This is "safer" in the event that the robot will
-      // always end the motion as soon as possible.
-      // However, if you require that BOTH motors have finished their moves before the robot continues
-      // onto the next step, use (isBusy() || isBusy()) in the loop test.
-      while (opModeIsActive() &&
-              (runtime.seconds() < timeoutS) &&
-              (bsgRobot.frontLeft.isBusy() && bsgRobot.frontRight.isBusy() &&
-                      bsgRobot.backLeft.isBusy() && bsgRobot.backRight.isBusy())) {
-
-        // Display it for the driver.
-        telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
-        telemetry.addData("Path2", "Running at %7d :%7d",
-                bsgRobot.frontLeft.getCurrentPosition(),
-                bsgRobot.backLeft.getCurrentPosition(),
-                bsgRobot.frontRight.getCurrentPosition(),
-                bsgRobot.backRight.getCurrentPosition());
-        telemetry.update();
-      }
-
-      // Stop all motion;
-      bsgRobot.stopWheels();
-
-      // Turn off RUN_TO_POSITION
-      bsgRobot.frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      bsgRobot.backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      bsgRobot.frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-      bsgRobot.backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-      //  sleep(250);   // optional pause after each move
-    }
   }
 
   public void moveForward(double power) {
@@ -254,30 +196,30 @@ public class Robot {
   }
 
   public void foundationDown(int pause) {
-    bsgRobot.rightFoundation.setPosition(.2);
-    bsgRobot.leftFoundation.setPosition(.8);
+    rightFoundation.setPosition(.2);
+    leftFoundation.setPosition(.8);
     sleep(pause);
   }
 
   public void foundationUp(int pause) {
-    bsgRobot.rightFoundation.setPosition(1);
-    bsgRobot.leftFoundation.setPosition(0);
+    rightFoundation.setPosition(1);
+    leftFoundation.setPosition(0);
     sleep(pause);
   }
 
   public void strafeLeft(long time) {
-    bsgRobot.frontRight.setPower(1);
-    bsgRobot.backRight.setPower(-1);
-    bsgRobot.frontLeft.setPower(-1);
-    bsgRobot.backLeft.setPower(1);
+    frontRight.setPower(1);
+    backRight.setPower(-1);
+    frontLeft.setPower(-1);
+    backLeft.setPower(1);
     sleep(time);
   }
 
   public void strafeRight(long time) {
-    bsgRobot.frontRight.setPower(-1);
-    bsgRobot.backRight.setPower(1);
-    bsgRobot.frontLeft.setPower(1);
-    bsgRobot.backLeft.setPower(-1);
+    frontRight.setPower(-1);
+    backRight.setPower(1);
+    frontLeft.setPower(1);
+    backLeft.setPower(-1);
     sleep(time);
   }
 }
